a195663a2a6ad4a2cb96f7ea038b63ec75434538
Branden Min <brandenmin@gmail.com>
2023-10-16 18:30:40 -0400

    experiences

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,6 +1,11 @@
 function Experiences() {
     return(
-        <div>Experiences</div>
+        <div>
+            <h1>Experiences</h1>
+            <h3>Data Analyst</h3>
+            <h4>Manse eSolutiuons Inc.</h4>
+
+        </div>
     )
 }
 
